46b689c26e63df7ae0413cf860969e7e4d667b4c
Victor Liu <victorliu716@gmail.com>
2019-09-30 21:03:59 -0500

    Implement first hook features!

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 const App = () => {
+  let [count, setCount] = useState(0)
+
   return (
     <div>
-      <p>The current count is 0</p>
+      <p>The current count is {count} </p>
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <button onClick={() => setCount(count = 0)}>reset</button>            
     </div>
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App count={0}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
