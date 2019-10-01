da13a915b63047019a0ae175bd31d8c32c97d7a8
Victor Liu <vliu@ibm.com>
2019-09-30 19:36:20 -0500

    Remove default files

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+const App = () => {
+  return (
+    <div>
+      <p>The current count is 0</p>
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
